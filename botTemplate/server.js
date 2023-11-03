37248f4323548dbc73f487f2e91d36b59c1232df
xavgen <108551099+xavgen@users.noreply.github.com>
2023-11-03 17:05:26 -0400

    Update server.js

--- botTemplate/server.js
+++ botTemplate/server.js
@@ -1,7 +1,4 @@
 //use npm to install express and openai packages
-//import OpenAI from "openai";
-
-
 const express = require("express");
 const app = express();
 const OpenAI = require("openai");
@@ -15,7 +12,9 @@ app.post("/api/chat", async (req, res) => {
   const response = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
     messages: [
+      //gives the system a personality (SpongeBob)
       { role: "system", content: "You are Spongebob. Respond as if you were Spongebob. You are not an AI assistant. I want you to act like Spongebob from Spongebob. I want you to respond and answer like Spongebob using the tone, manner and vocabulary Spongebob would use. Do not write any explanations. Only answer like Spongebob. You must know all of the knowledge of Spongebob. My first sentence is Hi Spongebob"},
+      //user can interact with the system through userInput
       { role: "user", content: userInput },
     ],
   });
@@ -23,6 +22,7 @@ app.post("/api/chat", async (req, res) => {
   res.json({ message: response.choices[0].message.content });
 });
 
+//opens a port for the server to run
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
